fe7f5e573ca8f778d38760cadb6aff15b86bc887
AlexMitrus <alejandrosilvabeltran@gmail.com>
2024-03-17 18:29:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".section");
-    let currentIndex = 0;
 
-    window.addEventListener("keydown", function (e) {
-        if (e.key === "ArrowDown" && currentIndex < sections.length - 1) {
-            currentIndex++;
-        } else if (e.key === "ArrowUp" && currentIndex > 0) {
-            currentIndex--;
-        }
-
-        scrollToSection(currentIndex);
-    });
-
-    function scrollToSection(index) {
-        const yOffset = index * 100;
-        document.getElementById("pinballMachine").style.transform = `translateY(-${yOffset}%)`;
-    }
-});
